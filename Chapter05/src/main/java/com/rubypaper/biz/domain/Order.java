c74dd569a25bc12aac91afb9244e5f5c5deba2fc
ready-oun <heeyn.lim@gmail.com>
2024-07-24 09:55:02 +0900

    chapter 5

---
+++ Chapter05/src/main/java/com/rubypaper/biz/domain/Order.java
@@ -0,0 +1,31 @@
+package com.rubypaper.biz.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "S_ORD")
+public class Order {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 주문 아이디
+	
+	@Column(name = "CUTOMER_ID")
+	private Long customerId;
+	
+	@Column(name = "ORDER_DATE")
+	private Date orderDate;
+	
+	private Double total; // 주문 금액
+	
+	@ManyToMany
+	@JoinTable(name="S_ITEM", joinColumns = @JoinColumn(name = "ORD_ID"), 
+		inverseJoinColumns = @JoinColumn(name = "PRODUCT_ID"), uniqueConstraints = @UniqueConstraint(columnNames = {"ORD_ID", "PRODUCT_ID"}))
+	private List<Product> productList = new ArrayList<Product>();
+}
